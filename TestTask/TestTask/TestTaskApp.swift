e3465aadef83973cefb0972b76346a9f438ddc86
Ivan Behichev <ivanbehichev@gmail.com>
2025-05-20 14:59:59 +0300

    initial commit

---
+++ TestTask/TestTask/TestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskApp.swift
+//  TestTask
+//
+//  Created by Ivan Behichev on 20.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
